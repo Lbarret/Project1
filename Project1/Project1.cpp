c2548bc5dd597f15a3a1dfdf8782984d6669ebad
Loic Barret <Loic@PLANETARY.local>
2019-08-30 19:03:39 -0400

    Add project files.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,18 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+
+	std::string username, password;
+	std::cout << "Enter Usesrname: ";
+	std::cin >> username;
+	std::cout << "Enter Password: ";
+	std::cin >> password;
+
+
+
+}
+
