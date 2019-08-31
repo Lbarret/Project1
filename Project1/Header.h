175c88416ef32cc00c4f640259648b432c1834db
Loic Barret <lbarret94@gmail.com>
2019-08-30 20:24:33 -0400

    first edit

--- Project1/Header.h
+++ Project1/Header.h
@@ -1,2 +1,30 @@
 #pragma once
+#include <fstream>
+#include <string>
+class Credentials {
 
+	
+
+public:
+	std::string username, password;
+	std::fstream userfile;
+	int offset;
+	bool chkuser;
+	
+
+	bool check_username() {
+
+		userfile.open("Usernames.txt");
+		std::getline(userfile, username);
+		if ((offset = username.find(username, 0)) != std::string::npos) {
+			chkuser = true;
+		}
+
+	}
+	void add_username() {
+		userfile.open("Usernames.txt");
+		userfile << username << "\n" << password;
+		userfile.close();
+	}
+	
+};
--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,18 +1,45 @@
-// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include "Header.h"
+#include <fstream>
+
 
 int main()
 {
+	std::ofstream myfile;
+	Credentials user;
+	std::string newuser, check_user, check_password;
 
-	std::string username, password;
-	std::cout << "Enter Usesrname: ";
-	std::cin >> username;
-	std::cout << "Enter Password: ";
-	std::cin >> password;
+	std::cout << "New User? (y/n)";
+	std::cin >> newuser;
+
+	if (newuser == "y") {
+		std::cout << "Enter Usesrname: ";
+		std::cin >> user.username;
+		std::cout << "Enter Password: ";
+		std::cin >> user.password;
+		
 
 
+		user.add_username();
 
+	}
+	
+	else{
+		std::cout << "Enter Usesrname: ";
+		std::cin >> check_user;
+		std::cout << "Enter Password: ";
+		std::cin >> check_password;
+
+		if (user.check_username()) {
+			std::cout << "Congrats! You're in!\n";
+			i++;
+		}
+
+		else {
+			std::cout << "Sorry, please try again\n";
+		}
+	}
 }
 
